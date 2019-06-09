418c72a77520ccc69bf87e1a92eba1e5fd2e4435
Tilak Raj <tilakraj1456@gmail.com>
2019-06-09 13:35:28 +0530

    change in lwc file

--- force-app/main/default/lwc/myComponent/myComponent.js
+++ force-app/main/default/lwc/myComponent/myComponent.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+import { LightningElement , track} from 'lwc';
 
-export default class MyComponent extends LightningElement {}
+export default class MyComponent extends LightningElement {
+    @track greeting = 'world';
+
+
+    handleNameChange(event){
+        this.greeting = event.target.value;
+        console.log('greeting onNamechange = '+ this.greeting);
+
+    }
+}
